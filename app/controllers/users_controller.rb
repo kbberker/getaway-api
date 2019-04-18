13101ed2e2c1e2d2d0defcae2bc8af63276e0b76
kbberker <kbbuk90@gmail.com>
2019-04-18 10:42:48 +0100

    Add user model with authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user
+      render json: @user
+    else
+      render json: {error: "User not found"}, status: 404
+    end
+  end
+
+  def create
+    @user = User.new(email: params[:username], first_name: params[:first_name], last_name: params[:last_name], password: params[:password])
+
+    if @user.save
+      render json: @user
+    else
+  render json: {error: "Unable to create user"}, status 400
+
+  end
+
+
+  def signin
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      render json: {message: 'it did work'}
+    else
+      render json: {error: "Username/password combination invalid."}, status: 400
+    end
+end
+
+end
